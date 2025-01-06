7569df10a54747f2ccea2220567c3e615eb64369
Rajudas575 <rajudas575@gmail.com>
2025-01-06 14:21:05 +0530

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-
-function UserList() {
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+ 
+const UserList = () => {
   const [users, setUsers] = useState([]);
-
-  useEffect(() => {
+ 
+  useEffect(()=>{
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => setUsers(data));
+    .then((response)=> response.json())
+    .then((data)=> setUsers(data))
+    .then((error)=>console.error("Error fetching users:", error));
   }, []);
-
   return (
     <div>
-      <h1>User List</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <Link to={`/users/${user.id}`}>{user.name}</Link>
-          </li>
-        ))}
-      </ul>
+    <h1>User List</h1>
+    <ul>
+      {users.map((user)=>(
+        <li key={user.id}>
+          <Link to={`/users/${user.id}`}>{user.name}</Link>
+        </li>
+      ))}
+    </ul>
     </div>
-  );
+  )
 }
-
-export default UserList;
+ 
+export default UserList
