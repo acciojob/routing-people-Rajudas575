2071172d4ae8b1e9a15c5353ef2e324b3f5fe0f3
Rajudas575 <rajudas575@gmail.com>
2025-01-06 14:20:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,18 @@
-
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import UserList from "./UserList";
-import UserDetails from "./UserDetails"
-import './../styles/App.css';
-
+import React from 'react'
+import UserDetails from './UserDetails'
+import UserList from './UserList'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+ 
+ 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-      <Router>
-        <div>
-          <h1>User Management</h1>
-          <Routes>
-            <Route path="/" element={<UserList />} />
-            <Route path="/users/:id" element={<UserDetails />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+  <Router>
+    <Routes>
+      <Route path='/' element={<UserList />} />
+      <Route path='/users/:id' element={<UserDetails />} />
+    </Routes>
+  </Router>
   )
 }
-
+ 
 export default App
